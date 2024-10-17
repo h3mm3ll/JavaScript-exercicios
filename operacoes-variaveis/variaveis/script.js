2ff6680fe90e82107b1745aaf002c9390cac9eeb
Luiza Hemmel Piovesan <hemmelpiovesanl@gmail.com>
2024-10-17 20:05:16 -0300

    criada pasta desafio-1-variaveis

---
+++ operacoes-variaveis/variaveis/script.js
@@ -0,0 +1,31 @@
+//operacoes com variaveis
+
+//operacoes matematicas
+
+let soma=78+459
+console.log(soma)
+
+let subtracao=111-22
+console.log(subtracao)
+
+let multiplicacao=444*444
+console.log(multiplicacao)
+
+let divisao=777777/444
+console.log(divisao)
+
+let sobra=33%2
+console.log(sobra)
+
+//operadores de comparacao
+let comparar= "2"==2
+console.log(comparar)
+
+let comparacaoidentica = "2" === 2
+console.log(comparacaoidentica)
+
+let maiorque =7>6
+console.log(maiorque)
+
+let maiorouigual = 6>=6
+console.log(maiorouigual)
---
+++ variaveis/script.js
@@ -0,0 +1,10 @@
+//tipos primitivos
+
+//number
+let idade=22
+console.log("a variavel " + idade + " do tipo number")
+
+//
+let estagravida=false
+tipo=typeof(estagravida)
+console.log("a variavel "+ estagravida + " do tipo " + tipo )
